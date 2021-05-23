98818e74048e5e5bad229758951a0564b09ee6ad
alva-selinder <alva.selinder@edu.nykoping.se>
2021-05-23 20:51:03 +0200

    scrolla till toppen-knapp fixad

--- topbutton.js
+++ topbutton.js
@@ -1,15 +1,23 @@
 //Get the button:
-button = document.getElementById("topButton");
+btn = document.getElementById("topButton");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        button.style.display = "block";
+
+    if (document.body.scrollTop < 20 || document.documentElement.scrollTop < 20) {
+        btn.classList.add("show")
     } else {
-        button.style.display = "none";
+        btn.classList.remove("show");
     }
+
+
+    //if (document.body.scrollTop < 20 || document.documentElement.scrollTop < 20) {
+        //button.style.display = "none";
+    //} else {
+        //button.style.display = "block";
+    //}
 }
 
 // When the user clicks on the button, scroll to the top of the document
